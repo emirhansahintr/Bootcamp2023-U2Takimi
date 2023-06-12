5382ec55cda7bbb4a8f7bd94efd14133efdc8585
emirhansahintr <emirhan.sahin.tr@gmail.com>
2023-06-12 22:05:05 +0300

    Setup

---
+++ Assets/Scripts/Character/Movement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float sprint = 18f;
+    public float walkspeed = 12f;
+    public float slowspeed = 3f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    Vector3 velocity;
+    public bool isGrounded;
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+        //Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        //Run
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded && speed == 12f)
+        {
+            speed = sprint;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = walkspeed;
+        }
+        //Slow
+        if (Input.GetKey(KeyCode.LeftControl) && isGrounded && speed == 12f)
+        {
+            speed = slowspeed;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            speed = walkspeed;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
